3ea773a9baea6edeccd29f7aac1a1a325f8e155a
marcos-senn <marcossenn@gmail.com>
2023-09-03 21:35:16 -0300

    logica terminada

--- index.js
+++ index.js
@@ -1,56 +1,119 @@
-const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-const symbols = ['-','.',',','!','?','@','#','$','%','&','*','+','=','/',';','(',')','[',']','{','}',':','<','>','|','_','~','`','^','"']
-const letters =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const symbols = [
+ "-",
+ ".",
+ ",",
+ "!",
+ "?",
+ "@",
+ "#",
+ "$",
+ "%",
+ "&",
+ "*",
+ "+",
+ "=",
+ "/",
+ ";",
+ "(",
+ ")",
+ "[",
+ "]",
+ "{",
+ "}",
+ ":",
+ "<",
+ ">",
+ "|",
+ "_",
+ "~",
+ "`",
+ "^",
+ '"',
+];
+const letters = [
+ "a",
+ "b",
+ "c",
+ "d",
+ "e",
+ "f",
+ "g",
+ "h",
+ "i",
+ "j",
+ "k",
+ "l",
+ "m",
+ "n",
+ "o",
+ "p",
+ "q",
+ "r",
+ "s",
+ "t",
+ "u",
+ "v",
+ "w",
+ "x",
+ "y",
+ "z",
+];
+const mayusLetters = [...letters].map((letter) => letter.toUpperCase());
 
-let passwordLength = 18
+let passwordLength = 6;
 let base = [];
-let password = ''
+let parametros = {
+ simbolos: false,
+ numeros: false,
+ mayus: false,
+};
 
+const random = () => {
+ let password = "";
+ for (let i = 0; i < passwordLength; i++) {
+  password += base[Math.floor(Math.random() * base.length)];
+ }
+ return password;
+};
 
-const generatePassword = (letras=false, simbolos=false, numeros= false) => {
-    if(letras&&simbolos&&numeros){
-     base = [...numbers, ...symbols, ...letters]
-     
-     for (let i = 0; i <= passwordLength; i++) {
-        password = password + base[Math.floor(Math.random() * base.length)]
-     }
-    }
+const generatePassword = ({ simbolos, numeros, mayus }) => {
+ switch (true) {
+  case simbolos && numeros && mayus:
+   base = [...letters, ...numbers, ...symbols, ...mayusLetters];
+   break;
 
-    else if(numeros&&simbolos){
-        base = [...numbers, ...symbols]
-        
-        for (let i = 0; i <= passwordLength; i++) {
-           password = password + base[Math.floor(Math.random() * base.length)]
-        }
-       }
+  case numeros && simbolos:
+   base = [...letters, ...numbers, ...symbols];
+   break;
 
-     else  if(numeros&&letras){
-        base = [...numbers, ...letters]
-        
-        for (let i = 0; i <= passwordLength; i++) {
-           password = password + base[Math.floor(Math.random() * base.length)]
-        }
-       }
+  case numeros && mayus:
+   base = [...letters, ...numbers, ...mayusLetters];
+   break;
 
-    else  if(simbolos&&letras){
-        base = [...symbols, ...letters]
-        
-        for (let i = 0; i <= passwordLength; i++) {
-           password = password + base[Math.floor(Math.random() * base.length)]
-        }
-       }
+  case simbolos && mayus:
+   base = [...letters, ...symbols, ...mayusLetters];
+   break;
 
-    else{
-        base = [...letters]
-        for (let i = 0; i <= passwordLength; i++) {
-            password = password + base[Math.floor(Math.random() * base.length)]
-         }
-    }
+  case numeros && simbolos:
+   base = [...letters, ...numbers, ...symbols];
+   break;
 
+  case mayus:
+   base = [...letters, ...mayusLetters];
+   break;
 
-    console.log(password)
-}
+  case numeros:
+   base = [...letters, ...numbers];
+   break;
 
+  case simbolos:
+   base = [...letters, ...symbols];
+   break;
 
+  default:
+   base = [...letters];
+ }
 
-generatePassword(letras=false,simbolos=false,numeros=false)
+ return random();
+};
